965a2857b80043307838ce0f7a25a5edc9e2f51c
Tony Baker <this.tonybaker@gmail.com>
2019-06-10 18:33:22 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,12 +2,12 @@ class dog
   attr_accessor :name, :breed, :age
 
   @@all = []
-  
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
     @@all << self
-    
+
 
 end
